7ec480ac644f0b008592f6d92ae98d7b3a3dec10
Raaga500 <zebratechnologiesindia@gmail.com>
2023-05-21 21:07:29 -0500

    addedAmmortizationCalculation

---
+++ ammortization.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+
+def PMT(rate, nper,pv, fv=0, type=0):
+    if rate!=0:
+        pmt = (rate*(fv+pv*(1+ rate)**nper))/((1+rate*type)*(1-(1+ rate)**nper))
+    else:
+        pmt = (-1*(fv+pv)/nper)  
+    return(pmt)
+
+
+def IPMT(rate, per, nper,pv, fv=0, type=0):
+  ipmt = -( ((1+rate)**(per-1)) * (pv*rate + PMT(rate, nper,pv, fv=0, type=0)) - PMT(rate, nper,pv, fv=0, type=0))
+  return(ipmt)
+
+
+def PPMT(rate, per, nper,pv, fv=0, type=0):
+  ppmt = PMT(rate, nper,pv, fv=0, type=0) - IPMT(rate, per, nper, pv, fv=0, type=0)
+  return(ppmt)
+
+def amortisation_schedule(amount, annualinterestrate, paymentsperyear, years):
+
+    df = pd.DataFrame({'Principal' :[PPMT(annualinterestrate/paymentsperyear, i+1, paymentsperyear*years, amount) for i in range(paymentsperyear*years)],
+                                 'Interest' :[IPMT(annualinterestrate/paymentsperyear, i+1, paymentsperyear*years, amount) for i in range(paymentsperyear*years)]})
+    
+    df['Instalment'] = df.Principal + df.Interest
+    df['Balance'] = amount + np.cumsum(df.Principal)
+    df['TotalPaid'] = amount  - df['Balance']
+    return(df)
+
+df = amortisation_schedule(50000, 0.085, 12, 6)
--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 import streamlit as st
 import math
+import pandas as pd
+import numpy as np
+import ammortization as am
 
+st. set_page_config(layout="wide")
 
-st.title("Mortgage Prepayment Calculator")
-st.divider()
-
-ord_mrtg_amt = st.text_input("**Original Mortgage Amount**",value=0.01).strip()
-org_mortg_term = st.text_input("Original Mortgage Term (in years)",value=0.01).strip()
-years_remaining = st.text_input("Years remaining",value=0.01).strip()
-interest_rate = st.text_input("Interest Rate",value=0.01).strip()
-add_principal_pmnt = st.text_input("Additional Principal Payment",value=0.01).strip()
-
-ord_mrtg_amt = float(ord_mrtg_amt)
-org_mortg_term = float(org_mortg_term)
-interest_rate = float(interest_rate)
-years_remaining = float(years_remaining)
-add_principal_pmnt = float(add_principal_pmnt)
-
-btn_pressed = st.button('Calculate')
+#st.title("Mortgage Prepayment Calculator")
+st.markdown("<h1 style='text-align: center;'>Mortgage Prepayment Calculator</h1>", unsafe_allow_html=True)
 
 st.divider()
+col1, col2 = st.columns(2)
+with col1:
+    ord_mrtg_amt = st.text_input("**Original Mortgage Amount**",value=212000).strip()
+    org_mortg_term = st.text_input("Original Mortgage Term (in years)",value=15).strip()
+    years_remaining = st.text_input("Years remaining",value=14).strip()
+    interest_rate = st.text_input("Interest Rate",value=4).strip()
+    add_principal_pmnt = st.text_input("Additional Principal Payment",value=176).strip()
 
-if btn_pressed:
+    ord_mrtg_amt = float(ord_mrtg_amt)
+    org_mortg_term = int(org_mortg_term)
+    interest_rate = float(interest_rate)
+    years_remaining = float(years_remaining)
+    add_principal_pmnt = float(add_principal_pmnt)
+
+    btn_pressed = st.button('Calculate')
+
+st.divider()
+
+def calculatePrepaymentNumbers(ord_mrtg_amt, interest_rate, years_remaining, add_principal_pmnt):
     numerator = ord_mrtg_amt * (interest_rate/1200 * math.pow((1 + interest_rate/1200), years_remaining*12))
     
     #st.write(numerator)
@@ -37,8 +44,36 @@ if btn_pressed:
     
     newTotalPayment = newMonthlyPayment * newTimePeriod
     totalInterestSaved = origTotalPayment - newTotalPayment
-    st.write('### Result:')
-    st.write(f'**Original Monthly Payment**: {round(monthlyPayment,2)}')
-    st.write(f'**New Monthly Payment**: {round(newMonthlyPayment,2)}')
-    st.write(f'**Months Saved**: {monthSaved}')
-    st.write(f'**Interest Saved**: {round(totalInterestSaved,2)}')
+
+    result_dict = {}
+    result_dict['monthlyPayment'] = monthlyPayment
+    result_dict['newMonthlyPayment'] = newMonthlyPayment
+    result_dict['monthSaved'] = monthSaved
+    result_dict['totalInterestSaved'] = totalInterestSaved
+    return result_dict
+
+def getAmmortizationSchedule(ord_mrtg_amt, interest_rate, org_mortg_term):
+    df = am.amortisation_schedule(ord_mrtg_amt,interest_rate/100.0,12,org_mortg_term)
+    return df
+
+if btn_pressed:
+    result_dict = calculatePrepaymentNumbers(ord_mrtg_amt,interest_rate,years_remaining,add_principal_pmnt)
+    with col2:
+        st.write("")
+        st.write("")
+        df = getAmmortizationSchedule(ord_mrtg_amt,interest_rate,org_mortg_term)
+        chart_data = df[['Balance','TotalPaid']]
+        st.line_chart(chart_data)
+        st.write('### Result:')
+        col2_a, col2_b = st.columns(2)
+        with col2_a:
+            st.write(f"**Original Monthly Payment**: {round(result_dict['monthlyPayment'],2)}")
+            st.write(f"**New Monthly Payment**: {round(result_dict['newMonthlyPayment'],2)}")
+        with col2_b:
+            st.write(f"**Months Saved**: {result_dict['monthSaved']}")
+            st.write(f"**Interest Saved**: {round(result_dict['totalInterestSaved'],2)}")
+        
+
+
+
+#st.write(df)
